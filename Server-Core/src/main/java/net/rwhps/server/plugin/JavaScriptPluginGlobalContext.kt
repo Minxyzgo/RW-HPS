37b69d5617846026e9db080f29eb165c45c15e03
Minxyzgo <71958008+Minxyzgo@users.noreply.github.com>
2024-06-10 04:21:13 +0800

    Update JavaScriptPluginGlobalContext.kt
    Signed-off-by: Minxyzgo <71958008+Minxyzgo@users.noreply.github.com>

--- Server-Core/src/main/java/net/rwhps/server/plugin/JavaScriptPluginGlobalContext.kt
+++ Server-Core/src/main/java/net/rwhps/server/plugin/JavaScriptPluginGlobalContext.kt
@@ -1,4 +1,4 @@
-/*
+`/*
  * Copyright 2020-2024 RW-HPS Team and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
@@ -169,6 +169,8 @@ internal class JavaScriptPluginGlobalContext {
                         it.option("js.webassembly", "true")
                     }
                 }
+                .allowCreateThread(true)
+                .allowAllAccess(true)
                 .allowHostAccess(
                         HostAccess.newBuilder()
                             .allowAllClassImplementations(true)
